65fc700474c57b2a1bdf102cceeb2129d93f4651
Leticia Cristina <letici4cr1s@gmail.com>
2025-06-03 02:17:21 -0300

    Update SistemaDebitoAutomatico.java

--- src/main/java/com/mycompany/sistemadebitoautomatico/SistemaDebitoAutomatico.java
+++ src/main/java/com/mycompany/sistemadebitoautomatico/SistemaDebitoAutomatico.java
@@ -42,39 +42,6 @@ public class SistemaDebitoAutomatico {
         } else {
             System.out.println("CNPJ não encontrado ou inválido.");
         }
-
-        System.out.println("\n===== Teste Classe Empresa =====");
-        Empresa empresaTeste = new Empresa();
-        empresaTeste.setEmpresaId(1);
-        empresaTeste.setNome("Minha Empresa LTDA");
-        empresaTeste.setCnpj("12345678000195");
-        empresaTeste.setCnpjValido(true);
-        empresaTeste.setRazaoSocial("Minha Empresa de Teste");
-        empresaTeste.setSituacaoCadastral("ATIVA");
-
-        System.out.println("Empresa ID: " + empresaTeste.getEmpresaId());
-        System.out.println("Nome: " + empresaTeste.getNome());
-        System.out.println("CNPJ: " + empresaTeste.getCnpj());
-        System.out.println("CNPJ Válido: " + empresaTeste.isCnpjValido());
-        System.out.println("Razão Social: " + empresaTeste.getRazaoSocial());
-        System.out.println("Situação Cadastral: " + empresaTeste.getSituacaoCadastral());
-        System.out.println("toString(): " + empresaTeste.toString());
-
-        System.out.println("\n===== Teste Classe DebitoAutomatico =====");
-        DebitoAutomatico debitoTeste = new DebitoAutomatico();
-        debitoTeste.setDebitoId(100);
-        debitoTeste.setEmpresaId(1);
-        debitoTeste.setValor(BigDecimal.valueOf(500.00));
-        debitoTeste.setDataAgendamento(LocalDate.now());
-        debitoTeste.setStatus("PENDENTE");
-
-        System.out.println("Débito ID: " + debitoTeste.getDebitoId());
-        System.out.println("Empresa ID: " + debitoTeste.getEmpresaId());
-        System.out.println("Valor: " + debitoTeste.getValor());
-        System.out.println("Data Agendamento: " + debitoTeste.getDataAgendamento());
-        System.out.println("Status: " + debitoTeste.getStatus());
-        System.out.println("toString(): " + debitoTeste.toString());
-
         
         scanner.close();
     }
